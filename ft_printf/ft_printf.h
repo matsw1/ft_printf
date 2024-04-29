3d8d14baf3960781a971ed449efd3bd2564ff96d
matsw1 <168378318+matsw1@users.noreply.github.com>
2024-04-29 03:57:01 +0200

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matemart <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 21:06:55 by matemart          #+#    #+#             */
+/*   Updated: 2023/09/30 17:09:51 by matemart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+int		ft_putnbr(int nb);
+int		ft_putunbr(unsigned int nb);
+int		ft_hexlower(unsigned int nb);
+int		ft_hexupper(unsigned int nb);
+int		ft_putpointer(void *nbr, char *base);
+
+#endif
---
+++ ft_printf/ft_puthex.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matemart <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 21:21:26 by matemart          #+#    #+#             */
+/*   Updated: 2023/09/30 16:46:19 by matemart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_hexlower(unsigned int nb)
+{
+	int		i;
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	i = 0;
+	if (nb >= 16)
+		i += ft_hexlower(nb / 16);
+	i += ft_putchar(hex[nb % 16]);
+	return (i);
+}
+
+int	ft_hexupper(unsigned int nb)
+{
+	int		i;
+	char	*hex;
+
+	hex = "0123456789ABCDEF";
+	i = 0;
+	if (nb >= 16)
+		i += ft_hexupper(nb / 16);
+	i += ft_putchar(hex[nb % 16]);
+	return (i);
+}
+
+int	ft_hexptr(unsigned long nb)
+{
+	int		i;
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	i = 0;
+	if (nb >= 16)
+		i += ft_hexptr(nb / 16);
+	i += ft_putchar(hex[nb % 16]);
+	return (i);
+}
